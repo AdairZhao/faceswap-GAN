fd8bb29a5bf8b2b1912134d5a1aa6752a1902211
shaoanlu <shaoanlu@gmail.com>
2018-01-18 18:02:28 +0800

    support 128x128 input/output size

--- image_augmentation.py
+++ image_augmentation.py
@@ -50,3 +50,25 @@ def random_warp( image ):
 
     return warped_image, target_image
 
+# get pair of random warped images from aligened face image
+def random_warp128(image):
+    assert image.shape == (256,256,3)
+    range_ = numpy.linspace(128-110, 128+110, 5)
+    mapx = numpy.broadcast_to(range_, (5,5))
+    mapy = mapx.T
+
+    mapx = mapx + numpy.random.normal(size=(5,5), scale=6)
+    mapy = mapy + numpy.random.normal(size=(5,5), scale=6)
+
+    interp_mapx = cv2.resize(mapx, (80*2,80*2))[8*2:72*2,8*2:72*2].astype('float32')
+    interp_mapy = cv2.resize(mapy, (80*2,80*2))[8*2:72*2,8*2:72*2].astype('float32')
+
+    warped_image = cv2.remap(image, interp_mapx, interp_mapy, cv2.INTER_LINEAR)
+
+    src_points = numpy.stack([mapx.ravel(), mapy.ravel() ], axis=-1)
+    dst_points = numpy.mgrid[0:65*2:16*2,0:65*2:16*2].T.reshape(-1,2)
+    mat = umeyama(src_points, dst_points, True)[0:2]
+
+    target_image = cv2.warpAffine(image, mat, (64*2,64*2))
+
+    return warped_image, target_image
